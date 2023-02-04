9961f12375cd77c784542e10ebc35e242654a705
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2023-02-04 02:35:43 +0100

    Basic enemy spawning system

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyToSpawn;
+    [SerializeField] private Transform enemySpawnerLocation;
+
+    public void Spawn()
+    {
+        if (!enemyToSpawn)
+        {
+            Debug.LogWarning(name + " - There is no enemy attached to this spawner!");
+            return;
+        }
+
+        if (!enemySpawnerLocation)
+        {
+            Debug.LogWarning(name + " - There is no enemySpawnerLocation set!");
+            return;
+        }
+
+        Instantiate(enemyToSpawn, enemySpawnerLocation.position, Quaternion.identity, transform);
+    }
+}
