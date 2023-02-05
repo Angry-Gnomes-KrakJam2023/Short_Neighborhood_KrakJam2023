ba324839385bcd78328eabbcfa6b32727bbd48b8
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2023-02-05 09:45:37 +0100

    audio volume sliders

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
     [SerializeField] private int mainSceneNumber;
+    [SerializeField] private AudioMixer audioMixer;
     
     public static Menu Singleton {get; private set;}
 
@@ -19,6 +22,13 @@ public class Menu : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+    }
+
+    private void Start()
+    {
+        audioMixer.SetFloat("musicVolume", Mathf.Log(PlayerPrefs.GetFloat("musicVolume", 0.5f)) * 20);
+        audioMixer.SetFloat("ambientVolume", Mathf.Log(PlayerPrefs.GetFloat("ambientVolume", 0.5f)) * 20);
+        audioMixer.SetFloat("sfxVolume", Mathf.Log(PlayerPrefs.GetFloat("sfxVolume", 0.5f)) * 20);
     }
 
     public void Play()
